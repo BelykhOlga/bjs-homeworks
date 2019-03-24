3c24bc3a9a5b03c1eda30422fe866e1d097371ae
Olga Belykh <belyholga88@gmail.com>
2019-03-25 00:17:01 +0500

    Result add

---
+++ variables/main.js
@@ -0,0 +1,21 @@
+let x, y, z;
+x = 2;
+y = 22;
+z = 0;
+
+let result;
+result = x * y + 5 * z + x - 1;
+console.log(result);
+
+let algebra, geography, physics;
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let average;
+average = (algebra + geography + physics) / 3;
+console.log(average);
+
+let name = 'Маша';
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
